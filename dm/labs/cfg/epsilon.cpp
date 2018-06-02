5d09c596bb3b9a72f51dde7c4b6057c9545d75d0
Konstantin <matveich1903@gmail.com>
2018-06-02 15:18:00 +0300

    bigint finally is finished

--- dm/labs/cfg/epsilon.cpp
+++ dm/labs/cfg/epsilon.cpp
@@ -9,41 +9,11 @@
 #include <unordered_map>
 #include <iterator>
 #include <sstream>
-
+#include <queue>
 
 using namespace std;
 
 typedef unsigned int ui;
-
-unordered_map<char, vector<string> > cfg;
-unordered_map<char, bool> eps;
-
-bool product_eps(char c) {
-    if (eps[c])
-        return true;
-    bool result = false;
-    for (auto &it: cfg[c]) {
-        bool flag = true;
-        for (auto& cr: it) {
-            if ('a' <= cr && cr <= 'z') {
-                flag = false;
-                break;
-            }
-        }
-        if (!flag)
-            continue;
-        result = true;
-        for (auto& cr: it) {
-            result &= product_eps(cr);
-            if (result)
-                break;
-        }
-        if (result)
-            break;
-    }
-    eps[c] = result;
-    return result;
-}
 
 vector<string> split_by_spaces(string& s) {
     istringstream iss(s);
@@ -54,27 +24,59 @@ vector<string> split_by_spaces(string& s) {
 int main() {
     freopen("epsilon.in", "r", stdin);
     freopen("epsilon.out", "w", stdout);
-    ios::sync_with_stdio(false);
+    std::ios_base::sync_with_stdio(false);
+    int n;
     char start = ' ';
-    ui n;
     cin >> n >> start;
-    string s = " ";
 
-    while(!cin.eof()) {
+    queue<char> q;
+
+    string s;
+    getline(cin, s);
+
+    vector<pair<char, int> > s_right((ui)n);
+    vector<bool> is_eps(26), marked(26);
+    vector<vector<int> > cfg(26);
+
+    for (int i = 0; i < n; i++) {
+        char c;
+        string right;
+
         getline(cin, s);
-        if (s.empty())
-            continue;
         auto ss = split_by_spaces(s);
+        c = ss[0][0] - 'A';
+
         if (ss.size() == 2) {
-            eps[ss[0][0]] = true;
-            cfg[ss[0][0]].clear();
+            q.push(c);
+            is_eps[c] = true;
+            continue;
         }
-        else
-            cfg[ss[0][0]].push_back(ss[2]);
+
+        s_right[i].first = c;
+        for (int j = 0; j < ss[2].size(); j++) {
+            s_right[i].second++;
+            if ('A' <= ss[2][j] && ss[2][j] <= 'Z')
+                cfg[ss[2][j] - 'A'].push_back(i);
+        }
     }
-    for (auto &it: cfg) {
-        if (product_eps(it.first))
-            cout << it.first << endl;
+
+    while (!q.empty()) {
+        auto v = q.front();
+        q.pop();
+        if (marked[v])
+            continue;
+        marked[v] = true;
+        for (auto &to: cfg[v]) {
+            s_right[to].second--;
+            if (s_right[to].second == 0) {
+                q.push(s_right[to].first);
+                is_eps[s_right[to].first] = true;
+            }
+        }
+    }
+    for (int i = 0; i < is_eps.size(); i++) {
+        if (is_eps[i])
+            cout << char('A' + i) << endl;
     }
     return 0;
 }
